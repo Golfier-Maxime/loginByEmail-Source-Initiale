2d138076c9aa82be173132e639ccfb1b8e7c612e
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2022-09-23 14:59:19 +0200

    connexion github

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
 
 <template>
   <header>
+    <h1>Maxime GOLFIER - MMI 2 - B2</h1>
     <router-link to="/" class="">Go to Login By Email</router-link>
     <br />
-    <router-link to="/tierce">Go to Login By Google</router-link>
+    <router-link to="/tierce">Go to Login By Google / GitHub</router-link>
     <img
       alt="Logo"
       class="logo"
--- src/pages/SignByGoogle.vue
+++ src/pages/SignByGoogle.vue
@@ -20,7 +20,8 @@ supabase.auth.onAuthStateChange((event, session) => {
   <div>
     <h1>{{ msg }}</h1>
     <p>Please login if you have an account or register :</p>
-    <button @click="login()">Sign In</button><br />
+    <button @click="login()">Sign In With Google</button><br />
+    <button @click="signInWithGithub()">Sign In With GitHub</button><br />
     <button @click="logout()">Sign Out</button><br />
     <label id="status">You are not yet logged ! </label>
   </div>
@@ -31,11 +32,14 @@ const SUPABASE_URL = "https://ncnkjckpjvogzcxeahvm.supabase.co";
 const SUPABASE_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jbmtqY2twanZvZ3pjeGVhaHZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMzMjkxMzUsImV4cCI6MTk3ODkwNTEzNX0.QV1t05RVYpPDVoIbYbC2ChMXrSdHuSdfd81AEMeqF3E";
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
+const { user, session, error } = await supabase.auth.signIn({
+  provider: "github",
+});
 export default {
   methods: {
     //this method allows a new user to sign up the system. Once done, the user receives an email
     //asking for account validation. Once the validation made the user is added to the system
+
     async register() {
       try {
         const { user, session, error } = await supabase.auth.signUp({
@@ -49,6 +53,7 @@ export default {
         alert(error.error_description || error.message);
       }
     },
+
     //this method allows the already registred user to log in the system.
     async login() {
       try {
@@ -74,6 +79,11 @@ export default {
       } catch (error) {
         alert(error.error_description || error.message);
       }
+    },
+    async signInWithGithub() {
+      const { user, session, error } = await supabase.auth.signIn({
+        provider: "github",
+      });
     },
   },
   mounted() {
